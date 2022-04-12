7e2f076fb3d5d37101e7edda4a8f91e3412baa7a
Johnson VILAYVANH <johnsonv@hotmail.fr>
2022-04-12 23:00:02 +0200

    Remove public GL::DynamicObject from Aircraft

--- src/aircraft.hpp
+++ src/aircraft.hpp
@@ -10,17 +10,17 @@
 #include <string>
 #include <string_view>
 
-class Aircraft : public GL::Displayable, public GL::DynamicObject
+class Aircraft : public GL::Displayable
 {
 private:
     const AircraftType& type;
     const std::string flight_number;
-    Point3D pos, speed; // note: the speed should always be normalized to length 'speed'
-    WaypointQueue waypoints = {};
+    Point3D           pos, speed; // note: the speed should always be normalized to length 'speed'
+    WaypointQueue     waypoints = {};
     Tower& control;
     bool landing_gear_deployed = false; // is the landing gear deployed?
     bool is_at_terminal        = false;
-
+    
     // turn the aircraft to arrive at the next waypoint
     // try to facilitate reaching the waypoint after the next by facing the
     // right way to this end, we try to face the point Z on the line spanned by
@@ -46,13 +46,14 @@ private:
 
 public:
     Aircraft(const AircraftType& type_, const std::string_view& flight_number_, const Point3D& pos_,
-             const Point3D& speed_, Tower& control_) :
-        GL::Displayable { pos_.x() + pos_.y() },
-        type { type_ },
-        flight_number { flight_number_ },
-        pos { pos_ },
-        speed { speed_ },
-        control { control_ }
+             const Point3D& speed_, Tower& control_)
+            :
+            GL::Displayable { pos_.x() + pos_.y() },
+            type { type_ },
+            flight_number { flight_number_ },
+            pos { pos_ },
+            speed { speed_ },
+            control { control_ }
     {
         speed.cap_length(max_speed());
     }
@@ -61,7 +62,7 @@ public:
     float distance_to(const Point3D& p) const { return pos.distance_to(p); }
 
     void display() const override;
-    bool move() override;
+    bool move();
 
     friend class Tower;
 };
